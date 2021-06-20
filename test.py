d3aa2929646ac2643ca671c3dadb10472eb740a1
Anita Hu <anitahu113@gmail.com>
2021-06-20 14:23:20 -0400

    lane evaluation from ultra fast

--- test.py
+++ test.py
@@ -1,40 +1,31 @@
 """
-Copyright (C) 2018 NVIDIA Corporation.  All rights reserved.
-Licensed under the CC BY-NC-SA 4.0 license (https://creativecommons.org/licenses/by-nc-sa/4.0/legalcode).
+Based on https://github.com/cfzd/Ultra-Fast-Lane-Detection/blob/master/test.py
 """
-from __future__ import print_function
-from utils import get_config, pytorch03_to_pytorch04
-from trainers import MUNIT_Trainer, UNIT_Trainer
 import argparse
-from torch.autograd import Variable
-import torchvision.utils as vutils
+import os
 import sys
 import torch
-import os
-from torchvision import transforms
-from PIL import Image
+from utils import get_config
+from trainers import MUNIT_Trainer, UNIT_Trainer
+from evaluation.eval_wrapper import eval_lane
+
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--config', type=str, help="net configuration")
 parser.add_argument('--input', type=str, help="input image path")
-parser.add_argument('--output_folder', type=str, help="output image path")
-parser.add_argument('--checkpoint', type=str, help="checkpoint of autoencoders")
-parser.add_argument('--style', type=str, default='', help="style image path")
-parser.add_argument('--a2b', type=int, default=1, help="1 for a2b and others for b2a")
+parser.add_argument('--output_folder', type=str, help="output folder for results")
+parser.add_argument('--checkpoint', type=str, help="checkpoint of autoencoders and lane model")
 parser.add_argument('--seed', type=int, default=10, help="random seed")
-parser.add_argument('--num_style',type=int, default=10, help="number of styles to sample")
-parser.add_argument('--synchronized', action='store_true', help="whether use synchronized style code or not")
-parser.add_argument('--output_only', action='store_true', help="whether use synchronized style code or not")
+parser.add_argument('--distributed', action='store_true', help="whether use distributed testing")
+parser.add_argument('--local_rank', type=int, default=0)
 parser.add_argument('--output_path', type=str, default='.', help="path for logs, checkpoints, and VGG model weight")
 parser.add_argument('--trainer', type=str, default='MUNIT', help="MUNIT|UNIT")
 opts = parser.parse_args()
 
 
-
 torch.manual_seed(opts.seed)
 torch.cuda.manual_seed(opts.seed)
-if not os.path.exists(opts.output_folder):
-    os.makedirs(opts.output_folder)
+torch.backends.cudnn.benchmark = True
 
 # Load experiment setting
 config = get_config(opts.config)
@@ -42,6 +33,7 @@ opts.num_style = 1 if opts.style != '' else opts.num_style
 
 # Setup model and data loader
 config['vgg_model_path'] = opts.output_path
+config['lane']['use_aux'] = False
 if opts.trainer == 'MUNIT':
     style_dim = config['gen']['style_dim']
     trainer = MUNIT_Trainer(config)
@@ -50,60 +42,35 @@ elif opts.trainer == 'UNIT':
 else:
     sys.exit("Only support MUNIT|UNIT")
 
-try:
-    state_dict = torch.load(opts.checkpoint)
-    trainer.gen_a.load_state_dict(state_dict['a'])
-    trainer.gen_b.load_state_dict(state_dict['b'])
-except:
-    state_dict = pytorch03_to_pytorch04(torch.load(opts.checkpoint))
-    trainer.gen_a.load_state_dict(state_dict['a'])
-    trainer.gen_b.load_state_dict(state_dict['b'])
+state_dict = torch.load(opts.checkpoint)
+# assume gen_a is for simulation data and gen_b is for real data
+trainer.gen_b.load_state_dict(state_dict['b'])
+trainer.lane_model.load_state_dict(state_dict['lane'])
 
 trainer.cuda()
 trainer.eval()
-encode = trainer.gen_a.encode if opts.a2b else trainer.gen_b.encode # encode function
-style_encode = trainer.gen_b.encode if opts.a2b else trainer.gen_a.encode # encode function
-decode = trainer.gen_b.decode if opts.a2b else trainer.gen_a.decode # decode function
 
-if 'new_size' in config:
-    new_size = config['new_size']
+if 'WORLD_SIZE' in os.environ:
+    distributed = int(os.environ['WORLD_SIZE']) > 1
+
+if distributed:
+    torch.cuda.set_device(opts.local_rank)
+    torch.distributed.init_process_group(backend='nccl', init_method='env://')
+
+print('start testing...')
+
+if config['datasetB'] == 'CULane':
+    cls_num_per_lane = 18
+elif config['datasetB'] == 'Tusimple':
+    cls_num_per_lane = 56
 else:
-    if opts.a2b==1:
-        new_size = config['new_size_a']
-    else:
-        new_size = config['new_size_b']
+    raise NotImplementedError
 
-with torch.no_grad():
-    transform = transforms.Compose([transforms.Resize(new_size),
-                                    transforms.ToTensor(),
-                                    transforms.Normalize((0.5, 0.5, 0.5), (0.5, 0.5, 0.5))])
-    image = Variable(transform(Image.open(opts.input).convert('RGB')).unsqueeze(0).cuda())
-    style_image = Variable(transform(Image.open(opts.style).convert('RGB')).unsqueeze(0).cuda()) if opts.style != '' else None
+if distributed:
+    net = torch.nn.parallel.DistributedDataParallel(trainer, device_ids=[opts.local_rank])
 
-    # Start testing
-    content, _ = encode(image)
+if not os.path.exists(opts.output_folder):
+    os.mkdir(opts.output_folder)
 
-    if opts.trainer == 'MUNIT':
-        style_rand = Variable(torch.randn(opts.num_style, style_dim, 1, 1).cuda())
-        if opts.style != '':
-            _, style = style_encode(style_image)
-        else:
-            style = style_rand
-        for j in range(opts.num_style):
-            s = style[j].unsqueeze(0)
-            outputs = decode(content, s)
-            outputs = (outputs + 1) / 2.
-            path = os.path.join(opts.output_folder, 'output{:03d}.jpg'.format(j))
-            vutils.save_image(outputs.data, path, padding=0, normalize=True)
-    elif opts.trainer == 'UNIT':
-        outputs = decode(content)
-        outputs = (outputs + 1) / 2.
-        path = os.path.join(opts.output_folder, 'output.jpg')
-        vutils.save_image(outputs.data, path, padding=0, normalize=True)
-    else:
-        pass
-
-    if not opts.output_only:
-        # also save input images
-        vutils.save_image(image.data, os.path.join(opts.output_folder, 'input.jpg'), padding=0, normalize=True)
-
+eval_lane(trainer, config['datasetB'], config['dataB_root'], opts.output_folder, config['lane']['griding_num'], False,
+          distributed)
