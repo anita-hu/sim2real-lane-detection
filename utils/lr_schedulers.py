29f5eb04ee9d0d8e1757e772fc62559a41a88010
Anita Hu <anitahu113@gmail.com>
2021-07-24 21:23:27 -0400

    added new lr schedulers, code cleanup

---
+++ utils/lr_schedulers.py
@@ -0,0 +1,92 @@
+# MIT License
+#
+# Copyright (c) 2020 cfzd (from https://github.com/cfzd/Ultra-Fast-Lane-Detection)
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
+import math
+
+
+class MultiStepLR:
+    def __init__(self, optimizer, steps, gamma=0.1, iters_per_epoch=None, warmup=None, warmup_iters=None):
+        self.warmup = warmup
+        self.warmup_iters = warmup_iters
+        self.optimizer = optimizer
+        self.steps = steps
+        self.steps.sort()
+        self.gamma = gamma
+        self.iters_per_epoch = iters_per_epoch
+        self.iters = 0
+        self.base_lr = [group['lr'] for group in optimizer.param_groups]
+
+    def step(self, external_iter=None):
+        self.iters += 1
+        if external_iter is not None:
+            self.iters = external_iter
+        if self.warmup == 'linear' and self.iters < self.warmup_iters:
+            rate = self.iters / self.warmup_iters
+            for group, lr in zip(self.optimizer.param_groups, self.base_lr):
+                group['lr'] = lr * rate
+            return
+
+        # multi policy
+        if self.iters % self.iters_per_epoch == 0:
+            epoch = int(self.iters / self.iters_per_epoch)
+            power = -1
+            for i, st in enumerate(self.steps):
+                if epoch < st:
+                    power = i
+                    break
+            if power == -1:
+                power = len(self.steps)
+
+            for group, lr in zip(self.optimizer.param_groups, self.base_lr):
+                group['lr'] = lr * (self.gamma ** power)
+
+    def get_last_lr(self):
+        return [group['lr'] for group in self.optimizer.param_groups]
+
+
+class CosineAnnealingLR:
+    def __init__(self, optimizer, T_max, eta_min=0, warmup=None, warmup_iters=None):
+        self.warmup = warmup
+        self.warmup_iters = warmup_iters
+        self.optimizer = optimizer
+        self.T_max = T_max
+        self.eta_min = eta_min
+
+        self.iters = 0
+        self.base_lr = [group['lr'] for group in optimizer.param_groups]
+
+    def step(self, external_iter=None):
+        self.iters += 1
+        if external_iter is not None:
+            self.iters = external_iter
+        if self.warmup == 'linear' and self.iters < self.warmup_iters:
+            rate = self.iters / self.warmup_iters
+            for group, lr in zip(self.optimizer.param_groups, self.base_lr):
+                group['lr'] = lr * rate
+            return
+
+        # cos policy
+        for group, lr in zip(self.optimizer.param_groups, self.base_lr):
+            group['lr'] = self.eta_min + (lr - self.eta_min) * (1 + math.cos(math.pi * self.iters / self.T_max)) / 2
+
+    def get_last_lr(self):
+        return [group['lr'] for group in self.optimizer.param_groups]
