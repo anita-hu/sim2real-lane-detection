b7c145ff355b4bebae2595136e2e734391bfc049
Anita Hu <anitahu113@gmail.com>
2022-01-26 22:07:17 -0500

    Demo visualization with support for classes (#39)
    * Run outputs directory now named using wandb ID
    
    * Changed folder name to run ID instead of run name
    
    * Added class pred to demo
    
    * small fixes
    
    Co-authored-by: MartinEthier <methier@uwaterloo.ca>

--- demo.py
+++ demo.py
@@ -13,39 +13,43 @@ import tqdm
 
 from data.dataloader import get_culane_row_anchor, get_tusimple_row_anchor
 from data.dataset import LaneTestDataset
-from trainers.baseline_trainer import Baseline_Trainer
-from trainers.munit_trainer import MUNIT_Trainer
-from trainers.unit_trainer import UNIT_Trainer
+from trainers import MUNIT_Trainer, UNIT_Trainer, Baseline_Trainer, ADA_Trainer
 from utils import get_config
+
+
+colormap = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
 
 
 def generate_video_demo(model, dataloader: torch.utils.data.DataLoader,
                         dataset_root: str, image_dims, model_in_dims, griding_num,
-                        outfile="demo.avi", framerate=30.0):
+                        outfile="demo.avi", framerate=30.0, use_cls=False):
     """
     Generates a video of sequential frames from the dataset, using the model
     to predict the lanes.
     :image_dims: (w, h) of the images used to create the video
     :model_in_dims: (w, h) of the model's input
     """
-
-
     outfile = os.path.join("outputs/", outfile)
     fourcc = cv2.VideoWriter_fourcc(*'MJPG')
-    vout = cv2.VideoWriter(outfile, fourcc , framerate, image_dims)
+    vout = cv2.VideoWriter(outfile, fourcc, framerate, image_dims)
 
     print(f"writing output to {outfile}.")
     for i, data in enumerate(tqdm.tqdm(dataloader)):
         imgs, names, _ = data
         imgs = imgs.cuda()
         with torch.no_grad():
-            out = model.eval_lanes(imgs)  # finally, run the images through the model.
+            det_out, cls_out, _ = model.eval_lanes(imgs)  # finally, run the images through the model.
 
-        for x in range(out.size()[0]):
+        if use_cls:
+            # Process classification output
+            cls_preds = torch.argmax(cls_out, dim=1).detach().cpu().numpy()
+
+        # Loop through each sample in batch
+        for x in range(det_out.size()[0]):
             col_sample = np.linspace(0, model_in_dims[0] - 1, griding_num)
             col_sample_w = col_sample[1] - col_sample[0]
 
-            out_j = out[x].data.cpu().numpy()
+            out_j = det_out[x].data.cpu().numpy()
             out_j = out_j[:, ::-1, :]
             prob = scipy.special.softmax(out_j[:-1, :, :], axis=0)
             idx = np.arange(griding_num) + 1
@@ -60,13 +64,23 @@ def generate_video_demo(model, dataloader: torch.utils.data.DataLoader,
             vis = cv2.imread(filename)  # read the original file from the dataset
             if vis is None:
                 raise ValueError(f"OpenCV failed to read a frame. Does the file {filename} exist?")
+            # Loop through each lane
             for i in range(out_j.shape[1]):
                 if np.sum(out_j[:, i] != 0) > 2:
+                    if use_cls:
+                        # Set color depending on class prediction
+                        cls_pred = cls_preds[x, i]
+                        color = colormap[cls_pred]
+                    else:
+                        # Default to green
+                        color = (0, 255, 0)
+
+                    # Loop through anchor locations
                     for k in range(out_j.shape[0]):
                         if out_j[k, i] > 0:
                             ppp = (int(out_j[k, i] * col_sample_w * image_dims[0] / model_in_dims[0]) - 1,
                                    int(row_anchor[cls_num_per_lane-1-k]) - 1)
-                            vis = cv2.circle(vis, ppp, 5, (0, 255, 0), -1)
+                            vis = cv2.circle(vis, ppp, 5, color, -1)
             vout.write(vis)  # write a frame
     vout.release()  # release lock for writing video
 
@@ -106,7 +120,7 @@ if __name__ == "__main__":
     elif cfg["dataset"] == 'TuSimple':
         cls_num_per_lane = 56
         splits = ['test.txt']
-        datasets = [LaneTestDataset(cfg["dataB_root"], os.path.join(cfg["dataB_root"], split),
+        datasets = [LaneTestDataset(cfg["dataB_root"], os.path.join(cfg["dataB_root"], 'list/'+split),
                                     img_transform=img_transforms) for split in splits]
         img_w, img_h = 1280, 720
         row_anchor = get_tusimple_row_anchor(img_h)
@@ -115,7 +129,6 @@ if __name__ == "__main__":
         # If you raise the framerate on TuSimple it's seizure-inducing
     else:
         raise NotImplementedError
-
 
     ############################################################################
     # Model
@@ -131,8 +144,10 @@ if __name__ == "__main__":
         trainer = UNIT_Trainer(cfg)
     elif cfg['trainer'] == 'Baseline':
         trainer = Baseline_Trainer(cfg)
+    elif cfg['trainer'] == 'ADA':
+        trainer = ADA_Trainer(cfg)
     else:
-        raise ValueError("Only support MUNIT|UNIT|Baseline")
+        raise ValueError("Only support MUNIT|UNIT|Baseline|ADA")
 
     state_dict = torch.load(opts.checkpoint)
     # assume gen_a is for simulation data and gen_b is for real data
@@ -146,7 +161,6 @@ if __name__ == "__main__":
     trainer.eval()
 
     for i, (split, dataset) in enumerate(zip(splits, datasets)):
-
         outfile = f"{opts.output_prefix}_{cfg['dataset']}_{split[:-4]}.avi"
 
         loader = torch.utils.data.DataLoader(dataset,
@@ -156,4 +170,5 @@ if __name__ == "__main__":
                             model_in_dims=(cfg["input_width"], cfg["input_height"]),
                             griding_num=cfg["lane"]["griding_num"],
                             outfile=outfile,
-                            framerate=framerate)
+                            framerate=framerate,
+                            use_cls=cfg['lane']['use_cls'])
